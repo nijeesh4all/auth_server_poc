aba3abe16a62067d7f672888379edcf0b434eba8
Nijeesh Joshy <nijeesh4all@gmail.com>
2024-11-24 16:13:52 +0530

    added root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def index
+    render html: (
+      <<-HTML
+                <h1> Hello, World! </h1>
+                <h3> Auth Server </h3>
+                <p> Please init the login from Client Application </p>
+            HTML
+    ).html_safe
+  end
+
 end
